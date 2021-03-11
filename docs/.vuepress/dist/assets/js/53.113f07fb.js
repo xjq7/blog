c1f0bdeaed40825719edba9753486768ba38c779
xjq <lebnizjoan@gmail.com>
2021-03-11 10:16:57 +0800

    feat: 清除dist

--- docs/.vuepress/dist/assets/js/53.113f07fb.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[53],{74:function(n,w,o){}}]);
--- docs/.vuepress/dist/assets/js/aaa.js
+++
@@ -1,1 +0,0 @@
-document.querySelector('.footer').innerHTML = '<a href="http://www.beian.miit.gov.cn">湘ICP备18011904号</a>'
